36693644cfc51239da8f6484208dbaa7b4338c7d
xshg <xshg@msn.com>
2018-12-08 10:24:18 +0800

    temp .gitignore

--- demo.py
+++ demo.py
@@ -4,8 +4,8 @@ import numpy as np
 # plt.figure(figsize=(5, 5))
 # plt.show()
 
-A = np.array([[1, 2],[3, 4]])
-B = np.array([[5, 6], [7, 8]])
+A = np.array([[1, 2, 3],[4, 5, 6]])
+B = np.array([[1, 2], [3, 4], [5, 6]])
 print(A)
 print(B)
 print(A * B)
